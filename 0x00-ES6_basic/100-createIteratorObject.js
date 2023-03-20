512774a7c5d1599893ae5448174b4f2a79948091
Stephen Oba <obastepheno@gmail.com>
2023-03-20 23:29:06 +0100

    ➕ Create an. Iterator for report object

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,28 @@
+function getEmployees(departments) {
+  const arr = [];
+  for (const dept of Object.values(departments)) {
+    arr.push(...dept);
+  }
+  return arr;
+}
+
+export default function createIteratorObject(report) {
+  const employees = getEmployees(report.allEmployees);
+  console.log(employees);
+  return {
+    [Symbol.iterator]() {
+      let counter = 0;
+      return {
+        next() {
+          if (counter < employees.length) {
+            counter += 1;
+            return { done: false, value: employees[counter - 1] };
+            /* eslint-disable no-else-return */
+          } else {
+            return { done: true, value: undefined };
+          }
+        },
+      };
+    },
+  };
+}
