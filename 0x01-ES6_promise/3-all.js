9ae6c440b794d95b7ef9d97a051a3dda295d1ba1
Stephen Oba <obastepheno@gmail.com>
2023-03-22 13:40:27 +0100

    🔧 fix error in task
    + Add a catch clause
    + Fix lint errors

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,11 @@
-import { uploadPhoto, createUser } from './utils'
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return (
     Promise.all([uploadPhoto(), createUser()])
-    .then((responses) => (
-      console.log(`${responses[0].body} ${responses[1].firstName} ${responses[1].lastName}`)
-    ))
-  )
+      .then((responses) => (
+        console.log(`${responses[0].body} ${responses[1].firstName} ${responses[1].lastName}`)
+      ))
+      .catch(() => console.error('Signup system offline'))
+  );
 }
