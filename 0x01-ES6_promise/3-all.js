b0ca39f76fde9b4c94cbc2bfa3cbadc1a5598f82
Stephen Oba <obastepheno@gmail.com>
2023-03-22 13:34:00 +0100

    ➕ Add file 3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils'
+
+export default function handleProfileSignup() {
+  return (
+    Promise.all([uploadPhoto(), createUser()])
+    .then((responses) => (
+      console.log(`${responses[0].body} ${responses[1].firstName} ${responses[1].lastName}`)
+    ))
+  )
+}
