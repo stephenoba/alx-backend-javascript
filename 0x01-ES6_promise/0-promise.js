368752cd495d23b12e0a1feb5cd7e336227dd282
Stephen Oba <obastepheno@gmail.com>
2023-03-22 12:37:25 +0100

    Task 0

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+export default function getResponseFromAPI() {
+  /* eslint-disable no-unused-vars */
+  /* eslint-disable func-names */
+  const promise = new Promise((resolve, reject) => {
+    /* Use resolve  and  reject */
+  });
+  return promise;
+}
