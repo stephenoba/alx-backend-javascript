af0c7086415eacefa4ecfc884ea89ef8b9886cec
Stephen Oba <obastepheno@gmail.com>
2023-03-22 13:45:17 +0100

    📝 Change console method

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,6 +6,6 @@ export default function handleProfileSignup() {
       .then((responses) => (
         console.log(`${responses[0].body} ${responses[1].firstName} ${responses[1].lastName}`)
       ))
-      .catch(() => console.error('Signup system offline'))
+      .catch(() => console.log('Signup system offline'))
   );
 }
