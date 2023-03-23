22db8fc06fa14bd2b5b4a7aed708cb639183a851
Stephen Oba <obastepheno@gmail.com>
2023-03-23 02:22:36 +0100

    🔧 Fix output and lint errors

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,8 +1,17 @@
-import {  uploadPhoto, createUser }  from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  return {
-    photo: await uploadPhoto(),
-    user: await createUser(),
+  let response;
+  try {
+    response = {
+      photo: await uploadPhoto(),
+      user: await createUser(),
+    };
+  } catch (e) {
+    response = {
+      photo: null,
+      user: null,
+    };
   }
+  return response;
 }
