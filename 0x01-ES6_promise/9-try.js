cf34c8747fae5910bb48b91afc944a7792d244db
Stephen Oba <obastepheno@gmail.com>
2023-03-23 02:02:00 +0100

    📝 Update output of function
    Add the error name to string

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,7 +3,7 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (e) {
-    queue.push(e.message);
+    queue.push(`${e.name}: ${e.message}`);
   }
   queue.push('Guardrail was processed');
   return queue;
