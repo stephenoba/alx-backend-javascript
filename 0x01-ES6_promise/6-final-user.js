bf6629627fef15ecc64cfab9c1663a9da7f8f325
Stephen Oba <obastepheno@gmail.com>
2023-03-23 01:14:11 +0100

    ➕ Add. file and test for task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,11 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return (
+    Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+      .then((responses) => (
+        [...responses]
+      ))
+  );
+}
