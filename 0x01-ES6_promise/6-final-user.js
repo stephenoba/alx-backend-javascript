aca64df3aeebd9f9071ad0d6c175a75fc017d011
Stephen Oba <obastepheno@gmail.com>
2023-03-23 02:28:45 +0100

    📝 Update function. to return all settled promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,5 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return (
-    Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-      .then((responses) => (
-        [...responses]
-      ))
-  );
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
 }
