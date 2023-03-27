cda99bfb108d03422f32b0ee730891cc60bbcb15
Stephen Oba <obastepheno@gmail.com>
2023-03-27 15:56:09 +0100

    Add class Building which implements an abstract method

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  /* eslint-disable class-methods-use-this */
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
