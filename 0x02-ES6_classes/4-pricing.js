5a720104309c7bc8adf894885d5b7c6d191a4f34
Stephen Oba <obastepheno@gmail.com>
2023-03-27 08:16:19 +0100

    Add file 4-pricing.js for task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,26 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+}
