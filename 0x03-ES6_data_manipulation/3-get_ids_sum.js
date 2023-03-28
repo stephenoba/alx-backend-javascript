2d2c99e2fad9434e35f823c1b7ee41091690799e
Stephen Oba <obastepheno@gmail.com>
2023-03-28 09:55:31 +0100

    ➕ Add solution for. task 3

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,10 @@
+export default function getStudentIdsSum(studentList) {
+  if (Array.isArray(studentList)) {
+    const initialValue = 0;
+    return studentList.map((student) => student.id).reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        initialValue
+    );
+  }
+  return [];
+}
