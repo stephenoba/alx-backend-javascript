9b1a1494ac435b2d84357c99220682165b80d6a5
Stephen Oba <obastepheno@gmail.com>
2023-03-28 09:57:18 +0100

    🔧 Fix lint errors

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,8 +2,8 @@ export default function getStudentIdsSum(studentList) {
   if (Array.isArray(studentList)) {
     const initialValue = 0;
     return studentList.map((student) => student.id).reduce(
-        (accumulator, currentValue) => accumulator + currentValue,
-        initialValue
+      (accumulator, currentValue) => accumulator + currentValue,
+      initialValue,
     );
   }
   return [];
