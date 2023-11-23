5c4f817e21b9ca9b1783e869e6639797d30b83a0
Stephen Oba <obastepheno@gmail.com>
2023-11-23 07:51:35 +0100

    Fix eslint errors

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,28 +1,31 @@
-const fs = require("fs");
+const fs = require('fs');
 
 const countStudents = (path) => {
-  let studentsCount = {};
+  const studentsCount = {};
   try {
-    const data = fs.readFileSync(path).toString("UTF8").split("\n");
+    const data = fs.readFileSync(path).toString('UTF8').split('\n');
     process.stdout.write(`Number of Students: ${data.length - 1}\n`);
-    for (let i = 1; i < data.length; i++) {
-      let [firstname, lastname, age, field] = data[i].split(",");
-      field = field.trim();
+    for (let i = 1; i < data.length; i += 1) {
+      const info = data[i].split(',');
+      const firstname = info[0];
+      const field = info[3].trim();
       if (!studentsCount[field]) {
-        studentsCount[field] = { count: 0, students: "" };
+        studentsCount[field] = { count: 0, students: '' };
       }
       studentsCount[field].count += 1;
       studentsCount[field].students = studentsCount[field].students
-        ? studentsCount[field].students + `, ${firstname}`
+        ? `${studentsCount[field].students}, ${firstname}`
         : firstname;
     }
   } catch (err) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
-  for (item in studentsCount) {
-    process.stdout.write(
-      `Number of students in ${item}: ${studentsCount[item].count}. List: ${studentsCount[item].students}\n`
-    );
+  for (const item in studentsCount) {
+    if (Object.hasOwn(studentsCount, item)) {
+      process.stdout.write(
+        `Number of students in ${item}: ${studentsCount[item].count}. List: ${studentsCount[item].students}\n`,
+      );
+    }
   }
 };
 
